013a65534fdcf951c0c5ca33c0727b7fb4f85789
nimbachi <nimbachi@unicauca.edu.co>
2026-04-04 19:35:29 -0500

    Building activity List

--- src/components/ActivityList.jsx
+++ src/components/ActivityList.jsx
@@ -1,8 +1,40 @@
-import React from "react";
+import {Grid, Typography, Card, CardContent} from "@mui/material";
+import React, { useEffect , useState} from "react";
+import {getActivities} from "../services/api";
+import {useNavigate} from "react-router";
 
 const ActivityList = () => {
+
+  const [activities, setActivities] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchActivities = async () => {
+      try {
+        const response = await getActivities();
+        setActivities(response.data);
+      } catch (error) {
+        console.error("Error fetching activities:", error);
+      }
+    };
+
+    fetchActivities();
+  }, []);
+
   return (
-    <div> Activity List </div>
+    <Grid container spacing={2} >
+      {activities.map((activity) => (
+        <Grid container spacing={{xs:2, md:3}} columns={{xs:4, sm:8, md:12}} >
+          <Card sx={{cursor: "pointer"}} onClick={() => navigate(`/activities/${activity.id}`)}>
+            <CardContent>
+              <Typography variant="h6">{activity.type}</Typography>
+              <Typography>Duration: {activity.duration}</Typography>
+              <Typography>Calories Burned: {activity.caloriesBurned}</Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+      ))}
+    </Grid>
   );
 }
 
